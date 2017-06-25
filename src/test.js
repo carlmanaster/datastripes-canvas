7935e9b861796046dcc9f64779b264d8b2a7dbc0
Carl Manaster <manaster@pobox.com>
2017-06-25 12:03:31 -0700

    adds offset example

--- src/test.js
+++ src/test.js
@@ -11,33 +11,41 @@ const makeTestCtx = (root, text) => {
 }
 
 const root = document.getElementById('root')
-let ctx
 
-const props = {
-  type: 'numeric',
-  width: 100
+const makeNumericSection = () => {
+  const props = {
+    type: 'numeric',
+    width: 100
+  }
+  let ctx
+
+  const h1 = document.createElement('h1')
+  root.appendChild(h1)
+  h1.innerText = 'Numeric Charts'
+
+  ctx = makeTestCtx(root, 'positive')
+  chart(props, testData.nonnegative, ctx)
+
+  ctx = makeTestCtx(root, 'high positive')
+  chart(props, testData.highpositive, ctx)
+
+  ctx = makeTestCtx(root, 'mixed')
+  chart(props, testData.mixed, ctx)
+
+  ctx = makeTestCtx(root, 'sorted')
+  chart(props, testData.sorted, ctx)
+
+  ctx = makeTestCtx(root, 'negative')
+  chart(props, testData.nonpositive, ctx)
+
+  ctx = makeTestCtx(root, 'high negative')
+  chart(props, testData.highnegative, ctx)
+
+  ctx = makeTestCtx(root, 'offset')
+  ctx.translate(100, 0)
+  chart(props, testData.sorted, ctx)
+
+  root.appendChild(document.createElement('hr'))
 }
 
-const NUMERIC = document.createElement('h1')
-root.appendChild(NUMERIC)
-NUMERIC.innerText = 'Numeric Charts'
-
-ctx = makeTestCtx(root, 'positive')
-chart(props, testData.nonnegative, ctx)
-
-ctx = makeTestCtx(root, 'high positive')
-chart(props, testData.highpositive, ctx)
-
-ctx = makeTestCtx(root, 'mixed')
-chart(props, testData.mixed, ctx)
-
-ctx = makeTestCtx(root, 'sorted')
-chart(props, testData.sorted, ctx)
-
-ctx = makeTestCtx(root, 'negative')
-chart(props, testData.nonpositive, ctx)
-
-ctx = makeTestCtx(root, 'high negative')
-chart(props, testData.highnegative, ctx)
-
-root.appendChild(document.createElement('hr'))
+makeNumericSection()
